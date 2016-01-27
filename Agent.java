e9210415b0abce8b669fb2be1f3fac39600bf2de
Etudiant <etu@r2d2.lan-213.stri>
2016-01-27 11:44:06 +0100

    Added comments to Agent and Manager - restructured the code a little bit

--- Agent.java
+++ Agent.java
@@ -1,6 +1,7 @@
 import java.net.MalformedURLException;
 import java.nio.channels.AlreadyBoundException;
 import java.rmi.Naming;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.server.UnicastRemoteObject;
@@ -12,7 +13,7 @@ public class Agent extends UnicastRemoteObject implements RMI_Int_Agent{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	
 
 	public Agent() throws RemoteException {
 		super();
@@ -24,9 +25,17 @@ public class Agent extends UnicastRemoteObject implements RMI_Int_Agent{
 	}
 
 
-	public static void main(String[] args) throws RemoteException, MalformedURLException, AlreadyBoundException, java.rmi.AlreadyBoundException  {
-		LocateRegistry.createRegistry(1099);
+	public static void main(String[] args) throws RemoteException, MalformedURLException, AlreadyBoundException, java.rmi.AlreadyBoundException, NotBoundException  {
+		// only one agent needs to do this, thus make a try/catch so that one dude does it and after it will output an error but we are going to ignore it
+		LocateRegistry.createRegistry(1099); 
+		
+		// enables this dude to listen through RMI, can throw an exception if agent is already registered in the registry : adds Agent_connection in RMI registry. Look up unbind maybe
+		// does not block the code
 		Naming.bind("Agent_connection", new Agent());
+		System.out.println("Bind done");
+		/*Naming.unbind("Agent_connection");
+		System.out.println("Unbind done");*/
+		
 	}
 
 }
--- Manager.java
+++ Manager.java
@@ -2,9 +2,14 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.Hashtable;
 
 public class Manager extends Object{
 
+	private int hierarchy; // int to know which priority this manager can manage
+	private Hashtable<String, String> hash_agents = new Hashtable<String, String>(); // hash managing agent list (contact list, name -> address)
+	
+	
 	public static void main(String args[]) throws MalformedURLException, RemoteException, NotBoundException {
 		Gestion g = new Gestion();
 		RMI_Int_Agent souche=(RMI_Int_Agent) Naming.lookup("rmi://localhost/Agent_connection");
