dab82b4746d523b3779f65c95a6c835a196d577f
Jeremy Michel <jeremy@jeremy.home>
2016-02-20 03:51:55 +0100

    added csv setter

--- Agent.java
+++ Agent.java
@@ -9,8 +9,11 @@ import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 //Represents an agent that is going to be connected to the manager
@@ -23,6 +26,71 @@ public class Agent extends UnicastRemoteObject implements RMI_Int_Agent{
 	private static ArrayList<Integer> ports = new ArrayList<Integer>();
 	private static Hashtable<Integer, String> portsTranslation = new Hashtable<Integer, String>();
 
+	
+	// First we read the initial line and make sure it exists
+	// Then, we buffer the whole file
+	// We modify the line with the new value (set)
+	// Finally, we rewrite the file
+	// returns -1 in case of error, 1 in case of success
+	private static int csvSetValue(String csvFilePath, String OID, String newValue) {
+		// vars
+		BufferedReader br = null;
+		BufferedWriter bw = null;
+		String[] existenceCheck = null;
+		boolean existence = false;
+		String line = "";
+		String newline = "";
+		String result[] = null;
+		
+		existenceCheck = csvLookup(csvFilePath, OID);
+		if (existenceCheck[0].equals(OID)) {
+			existence = true;
+		} else {
+			return -1;
+		}
+		
+		try {
+			File f = new File("/home/jeremy/M1STRI_Java_snmp/exampleOID.csv.bkp");
+			f.createNewFile();
+			
+			br = new BufferedReader(new FileReader(csvFilePath));
+			bw = new BufferedWriter(new FileWriter("/home/jeremy/M1STRI_Java_snmp/exampleOID.csv.bkp"));
+			while ((line = br.readLine()) != null) {
+				result = line.split(",");
+				if (result[0].equals(OID)) {
+					//this is the line that we want to modify
+					// Recreate it and then write it
+					newline = new StringBuilder().append(OID).append(",").append(newValue).toString();
+					bw.write(newline);
+					bw.newLine();
+				} else {
+					//this is the case where the targeted line of the set action isn't this line
+					bw.write(line);
+					bw.newLine();
+				}
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				br.close();
+				bw.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		// now swap the files
+		//TODO
+		return 1;
+	}
+	
+	
+	
+	
 	// searches the item "searchItem" and returns the line's fields
 	// Create a BufferedReader to read the file line by line
 	// If 'SearchItem' is found, return the split line
@@ -55,6 +123,7 @@ public class Agent extends UnicastRemoteObject implements RMI_Int_Agent{
 				e.printStackTrace();
 			}
 		}
+
 	// if no matches were found, the query is incorrect and thus we :
 	// 1 : empty the fields
 	// 2 : insert "Unknown item in the first field
@@ -107,6 +176,14 @@ public class Agent extends UnicastRemoteObject implements RMI_Int_Agent{
 		portsTranslation.put(445, "test2");
 		// =======================================
 		
+		String blbl[] = csvLookup("/home/jeremy/M1STRI_Java_snmp/example.csv", "agent2");
+		System.out.println("Field 1 : "+blbl[0]+"\nField 2 : "+blbl[1]+"\nField 3 : "+blbl[2]+"\nField 4 : "+blbl[3]);
+			
+		int vartest = csvSetValue("/home/jeremy/M1STRI_Java_snmp/exampleOID.csv", "1.2.3.4.1","wtfifthatworks");
+		System.out.println(vartest);
+		
+		
+		
 		// only one agent needs to do this
 		try {
 			LocateRegistry.createRegistry(1099);
@@ -116,8 +193,7 @@ public class Agent extends UnicastRemoteObject implements RMI_Int_Agent{
 		}
 		
 		Naming.bind("Agent_connection", new Agent());
-		String blbl[] = csvLookup("/home/jeremy/M1STRI_Java_snmp/example.csv", "agent2");
-		System.out.println("Field 1 : "+blbl[0]+"\nField 2 : "+blbl[1]+"\nField 3 : "+blbl[2]+"\nField 4 : "+blbl[3]);
+		
 		gestion = new Gestion(ports, portsTranslation);
 		
 		scannerThread = new PortScannerThread(gestion);
